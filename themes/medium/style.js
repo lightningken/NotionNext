119aea3c6f65652fecf024b78d77e249e688dae1
Lightningken <38026655+lightningken@users.noreply.github.com>
2025-02-14 14:58:26 +0800

    Update style.js

--- themes/medium/style.js
+++ themes/medium/style.js
@@ -6,15 +6,22 @@
  */
 const Style = () => {
   return <style jsx global>{`
+
     // 底色
     .dark body {
         background-color: black;
     }
-
-    // 隐藏日期
-    .flex.flex-wrap.text-gray-500.py-1 .whitespace-nowrap {
+    
+    // 隐藏特定的 div
+    .text-sm.py-1 {
         display: none;
     }
+    
+    // 隐藏日期和瀏覽
+    .flex.flex-wrap.text-gray-500.py-1 {
+        display: none;
+    }
+
   `}</style>
 }
 
